bc27d3cc8304c3f6cc4addcbafe49c74f623b3c4
GregoireLD <48733955+GregoireLD@users.noreply.github.com>
2020-03-15 13:46:46 +0100

    Reverting defaults Ecc to Low instead of Medium

--- PSQRCode.psm1
+++ PSQRCode.psm1
@@ -1842,7 +1842,7 @@ function New-QrCode {
 	param (
 		[Parameter(ParameterSetName="FromString")][string] $text="Sample",
 		[Parameter(ParameterSetName="FromSegments")][QrSegment[]] $segments,
-		[ValidateSet("LOW","MEDIUM","QUARTILE","HIGH")][string] $minimumEcc="MEDIUM",
+		[ValidateSet("LOW","MEDIUM","QUARTILE","HIGH")][string] $minimumEcc="LOW",
 		[ValidateRange(-1,7)][int] $forceMask=-1,
 		[switch] $disalowEccUpgrade
 	)
